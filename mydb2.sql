2632bdca8d46fe49aa92ba4f0672e7d0034557e1
Giorgi Olqiashvilli <143651250+GiorgiOlqiashvili@users.noreply.github.com>
2024-07-16 21:02:13 +0400

    Update mydb2.sql

--- mydb2.sql
+++ mydb2.sql
@@ -1,9 +1,9 @@
 
 CREATE TABLE Products (
     product_id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    category VARCHAR(50),
-    price DECIMAL(10, 2)
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
 );
 
 
